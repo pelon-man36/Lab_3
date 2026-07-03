d267a3a7921c4b454c4c55c69455e59b6d51de2b
Johan Ramirez <johand200505@gmail.com>
2026-07-03 12:55:54 -0400

    Created method process_file

--- Lab10_jramirez47-1.py
+++ Lab10_jramirez47-1.py
@@ -10,5 +10,11 @@ import json
 
 class WordAnalyzer:
     def __init__(self, file_path):
-        self.file_path = file_path
+        self.file_path = Path(file_path)
+
+    def process_file(self):
+        try:
+            self.file_path.exists()
+        except FileNotFoundError:
+            print(f"File not found")
     
