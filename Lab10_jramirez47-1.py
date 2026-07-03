df13b3352d4ed90628fa9e79afcd382bdf047336
Johan Ramirez <johand200505@gmail.com>
2026-07-03 12:50:50 -0400

    Imported json

--- Lab10_jramirez47-1.py
+++ Lab10_jramirez47-1.py
@@ -6,7 +6,9 @@ Uses 4 predefined files provided by the instructor
 07/02/2026
 """
 from pathlib import Path
+import json
 
 class WordAnalyzer:
     def __init__(self, file_path):
         self.file_path = file_path
+    
